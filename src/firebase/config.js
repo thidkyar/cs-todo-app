480dac3a30b5c66b6a1f1641e69f84efd1b4db58
thidkyar <thidkyar@gmail.com>
2021-05-22 13:07:35 -0400

    initialize firebase and setup folder structure

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCUSl9cnAKpgi2f-c4K04oogeQfkPo3-As",
+  authDomain: "cs-todo-app-b04c3.firebaseapp.com",
+  databaseURL: "https://cs-todo-app-b04c3-default-rtdb.firebaseio.com",
+  projectId: "cs-todo-app-b04c3",
+  storageBucket: "cs-todo-app-b04c3.appspot.com",
+  messagingSenderId: "261103000628",
+  appId: "1:261103000628:web:eb51c01a98da20706abe61",
+};
