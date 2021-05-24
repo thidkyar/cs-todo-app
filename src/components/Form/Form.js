46e8776f7bde1b6073ddabee14af793a5f584a06
Taranvir Hidkyar <thidkyar@gmail.com>
2021-05-24 15:15:09 -0400

    update form input placeholder text

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,47 +1,50 @@
 import React, { useState } from "react";
-import {Button, Modal, Form} from 'react-bootstrap'
+import { Button, Modal, Form } from "react-bootstrap";
 
 import firebase from "../../firebase/config";
-import './Form.css'
+import "./Form.css";
 
-const ToDoForm = ({show, setShow}) => {
-  const [title, setTitle] = useState("");
+const ToDoForm = ({ show, setShow }) => {
+    const [title, setTitle] = useState("");
 
-  const handleClose = () => setShow(false);
+    const handleClose = () => setShow(false);
 
-  const handleOnChange = (e) => {
-    setTitle(e.target.value);
-  };
-
-  const createTodo = () => {
-    const todoRef = firebase.database().ref("Todo");
-    const todo = {
-      title,
-      complete: false,
+    const handleOnChange = (e) => {
+        setTitle(e.target.value);
     };
 
-    todoRef.push(todo);
-    handleClose()
-  };
-  
-  return (
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Add To Do</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form.Control placeholder={title} onChange={handleOnChange}/>
-          </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={createTodo}>
-            Add
-          </Button>
-        </Modal.Footer>
-      </Modal>
-  );
+    const createTodo = () => {
+        const todoRef = firebase.database().ref("Todo");
+        const todo = {
+            title,
+            complete: false,
+        };
+
+        todoRef.push(todo);
+        handleClose();
+    };
+
+    return (
+        <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+                <Modal.Title>Add To Do</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form.Control
+                    placeholder="I need to.."
+                    onChange={handleOnChange}
+                />
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                    Close
+                </Button>
+                <Button variant="primary" onClick={createTodo}>
+                    Add
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    );
 };
 
 export default ToDoForm;
